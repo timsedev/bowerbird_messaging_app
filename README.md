687057bc8e85bf612f65bae4819bbdd4ae7701c5
timsedev <timsedev@gmail.com>
2024-01-05 14:52:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bowerbird_messaging_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/bowerbird_messaging_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bowerbird_messaging_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
