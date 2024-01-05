f2516e65d7a0d35adf50ce7e372abeb5c4a3fb76
timsedev <timsedev@gmail.com>
2024-01-05 17:40:46 +0800

    Add login functionality to LoginViewModel

--- lib/ui/views/login/login_viewmodel.dart
+++ lib/ui/views/login/login_viewmodel.dart
@@ -1,3 +1,18 @@
+import 'package:bowerbird_messaging_app/app/app.locator.dart';
+import 'package:bowerbird_messaging_app/services/auth_service.dart';
+import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
-class LoginViewModel extends BaseViewModel {}
+class LoginViewModel extends BaseViewModel {
+  final _authService = locator<AuthService>();
+
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  Future<void> login() async {
+    if (formKey.currentState!.validate()) {
+      await _authService.login(emailController.text, passwordController.text);
+    }
+  }
+}
