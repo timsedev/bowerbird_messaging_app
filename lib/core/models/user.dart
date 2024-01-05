501c5327c07b02a30e6b275b3515061bcb9864e7
timsedev <timsedev@gmail.com>
2024-01-05 15:55:25 +0800

    Refactor User model to include id, createdAt, updatedAt, and name

--- lib/core/models/user.dart
+++ lib/core/models/user.dart
@@ -1,22 +1,28 @@
 class User {
+  final String id;
   final String email;
-  final String password;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  final String name;
 
-  User({required this.email, required this.password});
+  User({
+    required this.id,
+    required this.email,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.name,
+  });
 
   factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      email: json['user']['email'],
-      password: json['user']['password'],
-    );
-  }
+    final createdAt = DateTime.parse(json['created_at']);
+    final updatedAt = DateTime.parse(json['updated_at']);
 
-  Map<String, dynamic> toJson() {
-    return {
-      'user': {
-        'email': email,
-        'password': password,
-      },
-    };
+    return User(
+      id: json['id'],
+      email: json['email'],
+      createdAt: createdAt,
+      updatedAt: updatedAt,
+      name: json['name'],
+    );
   }
 }
