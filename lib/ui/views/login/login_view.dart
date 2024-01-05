e17d354e1451df684fd4aae90047b23a90e1a3e8
timsedev <timsedev@gmail.com>
2024-01-05 17:41:49 +0800

    Added basic login ui

--- lib/ui/views/login/login_view.dart
+++ lib/ui/views/login/login_view.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
@@ -13,9 +15,40 @@ class LoginView extends StackedView<LoginViewModel> {
     Widget? child,
   ) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Form(
+        key: viewModel.formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: viewModel.emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your email';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: viewModel.passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Login'),
+              onPressed: () async => await viewModel.login(),
+            ),
+          ],
+        ),
       ),
     );
   }
