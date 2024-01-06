c3d41a34f8d47ae2a819ff38f483cb07027aad76
timsedev <timsedev@gmail.com>
2024-01-06 23:03:30 +0800

    Added getMessageGroups in MessageService

--- lib/services/message_service.dart
+++ lib/services/message_service.dart
@@ -1,1 +1,42 @@
-class MessageService {}
+import 'package:bowerbird_messaging_app/app/app.locator.dart';
+import 'package:bowerbird_messaging_app/core/models/message_group.dart';
+import 'package:bowerbird_messaging_app/core/models/response/message_group_response.dart';
+import 'package:bowerbird_messaging_app/ui/common/path.dart';
+import 'package:bowerbird_messaging_app/util/log_exception.dart';
+import 'package:dio/dio.dart';
+
+import 'auth_service.dart';
+
+class MessageService {
+  final _authService = locator<AuthService>();
+  final Dio _dio;
+
+  bool get isLoggedIn => _authService.user != null;
+  String? get token => _authService.token;
+
+  MessageService({Dio? dio}) : _dio = dio ?? Dio();
+
+  Future<List<MessageGroup>?> getMessageGroups() async {
+    try {
+      if (!isLoggedIn) {
+        return null;
+      }
+
+      final response = await _dio.get(
+        Path.indexMessageGroups,
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+
+      if (response.statusCode == 200) {
+        final messageGroupsResponse =
+            MessageGroupResponse.fromJson(response.data);
+        return messageGroupsResponse.messageGroups;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      logException(e);
+      return null;
+    }
+  }
+}
