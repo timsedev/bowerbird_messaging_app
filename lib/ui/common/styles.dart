6991ad6cd822cb54c6095b382482d9faad043d55
timsedev <timsedev@gmail.com>
2024-01-08 17:50:40 +0800

    Added comment to view value when hovering in other context.

--- lib/ui/common/styles.dart
+++ lib/ui/common/styles.dart
@@ -4,60 +4,158 @@ import 'package:flutter/material.dart';
 const double kBorderRadius = 12.0;
 
 // sizes
+/// 4.0
 const double size2XS = 4.0;
+
+/// 8.0
 const double sizeXS = 8.0;
+
+/// 12.0
 const double sizeS = 12.0;
+
+/// 16.0
 const double sizeM = 16.0;
+
+/// 24.0
 const double sizeL = 24.0;
+
+/// 32.0
 const double sizeXL = 32.0;
+
+/// 48.0
 const double size2XL = 48.0;
-const double size3XL = 48.0;
-const double size4XL = 64.0;
+
+/// 64.0
+const double size3XL = 64.0;
+
+/// 80.0
+const double size4XL = 80.0;
+
+/// 96.0
+const double size5XL = 96.0;
+
+/// 112.0
+const double size6XL = 112.0;
+
+/// 128.0
+const double size7XL = 128.0;
+
+/// 144.0
+const double size8XL = 144.0;
+
+/// 160.0
+const double size9XL = 160.0;
 
 // spaces
+/// 4.0
 const Widget horizontalSpace2XS = SizedBox(width: size2XS);
+
+/// 8.0
 const Widget horizontalSpaceXS = SizedBox(width: sizeXS);
+
+/// 12.0
 const Widget horizontalSpaceS = SizedBox(width: sizeS);
+
+/// 16.0
 const Widget horizontalSpaceM = SizedBox(width: sizeM);
+
+/// 24.0
 const Widget horizontalSpaceL = SizedBox(width: sizeL);
+
+/// 32.0
 const Widget horizontalSpaceXL = SizedBox(width: sizeXL);
+
+/// 48.0
 const Widget horizontalSpace2XL = SizedBox(width: size2XL);
+
+/// 64.0
 const Widget horizontalSpace3XL = SizedBox(width: size3XL);
+
+/// 80.0
 const Widget horizontalSpace4XL = SizedBox(width: size4XL);
 
+/// 4.0
 const Widget verticalSpace2XS = SizedBox(height: size2XS);
+
+/// 8.0
 const Widget verticalSpaceXS = SizedBox(height: sizeXS);
+
+/// 12.0
 const Widget verticalSpaceS = SizedBox(height: sizeS);
+
+/// 16.0
 const Widget verticalSpaceM = SizedBox(height: sizeM);
+
+/// 24.0
 const Widget verticalSpaceL = SizedBox(height: sizeL);
+
+/// 32.0
 const Widget verticalSpaceXL = SizedBox(height: sizeXL);
+
+/// 48.0
 const Widget verticalSpace2XL = SizedBox(height: size2XL);
+
+/// 64.0
 const Widget verticalSpace3XL = SizedBox(height: size3XL);
+
+/// 80.0
 const Widget verticalSpace4XL = SizedBox(height: size4XL);
 
 // colors
 const Color kPrimaryColor = Color(0xFF333333);
-const Color kWhite = Color(0xFFFFFFFF);
+const Color kWhiteColor = Color(0xFFFFFFFF);
 const Color kAccentColor = Color(0xFFE2FF32);
 const Color kErrorColor = Color(0xFFFF4949);
 
 // fontsizes
+/// 10.0
 const double kFontSize2XS = 10.0;
+
+/// 12.0
 const double kFontSizeXS = 12.0;
+
+/// 14.0
 const double kFontSizeS = 14.0;
+
+/// 16.0
 const double kFontSizeM = 16.0;
+
+/// 18.0
 const double kFontSizeL = 18.0;
+
+/// 20.0
 const double kFontSizeXL = 20.0;
+
+/// 24.0
 const double kFontSize2XL = 24.0;
+
+/// 30.0
 const double kFontSize3XL = 30.0;
+
+/// 36.0
 const double kFontSize4XL = 36.0;
 
 // fontweights
+/// 200
 const FontWeight kFontWeightExtraLight = FontWeight.w200;
+
+/// 300
 const FontWeight kFontWeightLight = FontWeight.w300;
+
+/// 400
 const FontWeight kFontWeightRegular = FontWeight.w400;
+
+/// 500
 const FontWeight kFontWeightMedium = FontWeight.w500;
+
+/// 600
 const FontWeight kFontWeightSemiBold = FontWeight.w600;
+
+/// 700
 const FontWeight kFontWeightBold = FontWeight.w700;
+
+/// 800
 const FontWeight kFontWeightExtraBold = FontWeight.w800;
+
+/// 900
 const FontWeight kFontWeightBlack = FontWeight.w900;
