2889041ac15d5e59cda3dc4276f0454f50ee716f
timsedev <timsedev@gmail.com>
2024-01-08 22:13:28 +0800

    Added token setter and getter

--- lib/services/secure_storage_service.dart
+++ lib/services/secure_storage_service.dart
@@ -1,1 +1,15 @@
-class SecureStorageService {}
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorageService {
+  final _storage = const FlutterSecureStorage();
+
+  final _tokenKey = 'jwt-token-key';
+
+  Future<void> setToken(String token) async {
+    await _storage.write(key: _tokenKey, value: token);
+  }
+
+  Future<String?> getToken() async {
+    return await _storage.read(key: _tokenKey);
+  }
+}
