f68ff1b22cf92782a3ee42588a55597269fccc27
timsedev <timsedev@gmail.com>
2024-01-08 17:47:43 +0800

    Added BowerBird logo widget

---
+++ lib/ui/common/logo.dart
@@ -0,0 +1,31 @@
+import 'package:bowerbird_messaging_app/ui/common/styles.dart';
+import 'package:flutter/material.dart';
+
+Widget bowerBirdLogo({required double size, required isDark}) {
+  return Container(
+    height: size,
+    width: size,
+    decoration: BoxDecoration(
+      color: isDark ? kPrimaryColor : kWhiteColor,
+      shape: BoxShape.circle,
+    ),
+  );
+}
+
+Widget bowerBirdLogoAndText({required double size, required isDark}) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      bowerBirdLogo(size: size, isDark: isDark),
+      horizontalSpaceXS,
+      Text(
+        'Bowerbird',
+        style: TextStyle(
+          fontSize: size,
+          fontWeight: kFontWeightBold,
+          color: isDark ? kPrimaryColor : kWhiteColor,
+        ),
+      ),
+    ],
+  );
+}
