660604fc2f1c3e7e958f741fbe2c5ce791345585
timsedev <timsedev@gmail.com>
2024-01-08 17:59:22 +0800

    Added toggleVisibility method and isVisible property

--- lib/ui/views/login/login_viewmodel.dart
+++ lib/ui/views/login/login_viewmodel.dart
@@ -1,6 +1,7 @@
 import 'package:bowerbird_messaging_app/app/app.locator.dart';
 import 'package:bowerbird_messaging_app/app/app.router.dart';
 import 'package:bowerbird_messaging_app/services/auth_service.dart';
+import 'package:bowerbird_messaging_app/ui/common/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -13,10 +14,18 @@ class LoginViewModel extends BaseViewModel {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
+  bool isVisible = false;
+
+  void toggleVisibility() {
+    isVisible = !isVisible;
+    notifyListeners();
+  }
+
   Future<void> login() async {
     if (!formKey.currentState!.validate()) {
       return;
     }
+    setBusy(true);
     final isSuccess =
         await _authService.login(emailController.text, passwordController.text);
 
@@ -27,9 +36,11 @@ class LoginViewModel extends BaseViewModel {
           .showSnackBar(
         const SnackBar(
           content: Text('Login failed'),
-          backgroundColor: Colors.red,
+          backgroundColor: kErrorColor,
         ),
       );
     }
+
+    setBusy(false);
   }
 }
