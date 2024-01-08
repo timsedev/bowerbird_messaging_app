95d0214de9229147611e23eedc1daec7f29197f6
timsedev <timsedev@gmail.com>
2024-01-08 22:24:39 +0800

    Generated simple splash screen with BowerBird logo

--- lib/ui/views/startup/startup_view.dart
+++ lib/ui/views/startup/startup_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:stacked/stacked.dart';
-import 'package:bowerbird_messaging_app/ui/common/ui_helpers.dart';
 
 import 'startup_viewmodel.dart';
 
@@ -18,27 +17,7 @@ class StartupView extends StackedView<StartupViewModel> {
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'STACKED',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Loading ...', style: TextStyle(fontSize: 16)),
-                horizontalSpaceSmall,
-                SizedBox(
-                  width: 16,
-                  height: 16,
-                  child: CircularProgressIndicator(
-                    color: Colors.black,
-                    strokeWidth: 6,
-                  ),
-                )
-              ],
-            ),
-          ],
+          children: [],
         ),
       ),
     );
